d8b1b85afee258fce3207903d5e618d777c1a7fb
Jonathan Cheng <jcheng461@gmail.com>
2022-11-08 23:18:17 +0900

    set up help queue project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h3>Header</h3>
+      <h3>Ticket Control</h3>
+    </React.Fragment>
+  );
+}
+
+export default App;
